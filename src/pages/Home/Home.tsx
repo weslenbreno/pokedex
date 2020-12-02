49551417f508571daabbdfc969e2eed1bf9056fe
Weslen Breno <weslenbreno@gmail.com>
2020-12-02 16:32:33 -0300

    Create home

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Header } from 'components';
+import { Container } from './styles';
+
+const Home = () => {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+};
+
+export default Home;
