dda9d0a349af96d4f8829e805fc6d1b71ead37c5
Weslen Breno <weslenbreno@gmail.com>
2020-12-02 16:33:43 -0300

    Header component

---
+++ src/components/PokeCard/PokeCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PokeCard = () => {
+  return <div></div>;
+};
+
+export default PokeCard;
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export {default as LogoHeader} from './LogoHeader/LogoHeader'
+export {default as Header} from './Header/Header'
+export {default as SearchBar} from './SearchBar/SearchBar'
