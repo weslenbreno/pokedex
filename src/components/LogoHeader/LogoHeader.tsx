e338926af311ccb13a4ed5184c9d4db15389150a
Weslen Breno <weslenbreno@gmail.com>
2020-12-02 16:32:54 -0300

    Create logo component

--- src/components/LogoHeader/LogoHeader.tsx
+++ src/components/LogoHeader/LogoHeader.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
+import pokeball from 'assets/images/pokeball.svg';
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0px 15px;
+  h1 {
+    margin-left: 8px;
+    font-family: 'SF Pro Black';
+    font-size: 22px;
+  }
+`;
 
 const LogoHeader = () => {
-  return <div></div>;
+  return (
+    <Logo>
+      <img src={pokeball} alt="pokeball" height={35} />
+      <h1>Pockédex</h1>
+    </Logo>
+  );
 };
 
 export default LogoHeader;
