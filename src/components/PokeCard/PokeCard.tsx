0a6decd0172f3d40bcd0bf0aedcba9604f5f152a
Weslen Breno <weslenbreno@gmail.com>
2020-12-04 01:57:51 -0300

    Better cards integration

--- src/components/PokeCard/PokeCard.tsx
+++ src/components/PokeCard/PokeCard.tsx
@@ -1,79 +1,18 @@
 import React from 'react';
-import { ThemeColors } from 'shared/constants/Colors';
-import styled from 'styled-components';
-import egg from 'assets/images/egg.png';
 import Lottie from 'lottie-react';
 import cardAnimation from 'assets/animations/card.json';
-import { Link } from 'react-router-dom';
-import { useFetch } from 'hooks/usefecth';
+import useFetch from 'hooks/useFecth';
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addPokemonToFavorites, removePokemonFromFavorites } from 'store/ducks/pokedex';
+import { useSelector } from 'react-redux';
+import { RootState } from 'store/store';
+import { addFallbackImage } from 'utils/functions';
 
-const Card = styled.div`
-  position: relative;
-  display: flex;
-  flex-wrap: wrap;
-  width: 300px;
-  background-color: ${ThemeColors.white};
-  border-radius: 30px;
-  margin: 25px;
-  box-shadow: 0px 3px 7px rgba(0, 0, 0, 0.16);
-  @media (max-width: 800px) {
-    flex-grow: 1;
-  }
-`;
-const CardBody = styled.div`
-  padding: 25px;
-  padding-bottom: 50px;
+import { Card, CardBody, CardFooter, FooterBtn, PokeID, PokeImg, PokeTypes } from './styles';
 
-  h2 {
-    font-family: 'SF Pro Black';
-    font-size: 18px;
-    text-transform: capitalize;
-  }
-`;
-
-const PokeID = styled.span`
-  font-family: 'SF Pro SemiBold';
-  font-size: 18px;
-  color: ${ThemeColors.darkGrey};
-`;
-
-const CardFooter = styled.div`
-  background-color: ${ThemeColors.bg};
-  border-radius: 0px 0px 30px 30px;
-  box-shadow: 0px 3px 7px rgba(0, 0, 0, 0.16);
-  height: 50px;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  align-self: flex-end;
-  padding: 0px 25px;
-`;
-
-const FooterBtn = styled(Link)`
-  text-decoration: none;
-  span {
-    font-family: 'SF Pro Medium';
-    color: ${ThemeColors.lightBlue};
-    font-size: 14px;
-  }
-`;
-
-const PokeTypes = styled.span`
-  background-color: ${ThemeColors.bg};
-  padding: 5px 20px;
-  border-radius: 25px;
-  font-family: 'SF Pro Light';
-  font-size: 13px;
-  margin-right: 8px;
-`;
-
-const PokeImg = styled.img`
-  position: absolute;
-  top: -35px;
-  right: 35px;
-  height: 90px;
-`;
+import starImg from 'assets/images/star.svg';
+import addImg from 'assets/images/add.svg';
 
 type Props = {
   fetchUrl: string;
@@ -82,11 +21,17 @@ type Props = {
 const PokeCard: React.FC<Props> = ({ fetchUrl }) => {
   const match = fetchUrl.match(/\/pokemon\/(\d+)\//);
   const id = match ? match[1] : 0;
-
+  const history = useHistory();
   const { data } = useFetch(fetchUrl);
+  const dispatch = useDispatch();
+  const favorites = useSelector((state: RootState) => state.app.favorites);
 
-  const addFallbackImage = (event: any) => {
-    event.target.src = data?.sprites?.front_default || egg;
+  const addToFavorite = () => {
+    dispatch(addPokemonToFavorites(data));
+  };
+
+  const removeFromFavorite = () => {
+    dispatch(removePokemonFromFavorites(data));
   };
 
   return (
@@ -94,21 +39,30 @@ const PokeCard: React.FC<Props> = ({ fetchUrl }) => {
       {data ? (
         <Card>
           <PokeImg
-            onError={addFallbackImage}
+            onError={(event) => addFallbackImage(event, data)}
             src={`https://pokeres.bastionbot.org/images/pokemon/${id}.png`}
           />
           <CardBody>
             <PokeID># {id}</PokeID>
             <h2>{data?.name}</h2>
             {data?.types.map(({ type }: any) => (
-              <PokeTypes key={type.name}>{type.name}</PokeTypes>
+              <PokeTypes key={type?.name}>{type.name}</PokeTypes>
             ))}
           </CardBody>
           <CardFooter>
-            <FooterBtn>
-              <span>Favoritar</span>
-            </FooterBtn>
-            <FooterBtn to={`/detail/${id}`}>
+            {!favorites?.some((item: any) => item?.id === data?.id) ? (
+              <FooterBtn type="button" onClick={addToFavorite}>
+                <img src={addImg} height={22} alt="Star" />
+
+                <span>Favoritar</span>
+              </FooterBtn>
+            ) : (
+              <FooterBtn type="button" onClick={removeFromFavorite}>
+                <img src={starImg} height={22} alt="Star" />
+                <span>Desfavoritar</span>
+              </FooterBtn>
+            )}
+            <FooterBtn onClick={() => history.push(`/detail/${id}`)}>
               <span>Detalhes</span>
             </FooterBtn>
           </CardFooter>
---
+++ src/components/PokeCard/styles.ts
@@ -0,0 +1,85 @@
+import { ThemeColors } from 'shared/constants/Colors';
+import styled from 'styled-components';
+
+export const Card = styled.div`
+  position: relative;
+  display: flex;
+  flex-wrap: wrap;
+  width: 300px;
+  background-color: ${ThemeColors.white};
+  border-radius: 30px;
+  margin: 25px;
+  box-shadow: 0px 3px 7px rgba(0, 0, 0, 0.16);
+  @media (max-width: 800px) {
+    flex-grow: 1;
+  }
+`;
+export const CardBody = styled.div`
+  padding: 25px;
+  padding-bottom: 50px;
+
+  h2 {
+    font-family: 'SF Pro Black';
+    font-size: 18px;
+    text-transform: capitalize;
+  }
+`;
+
+export const PokeID = styled.span`
+  font-family: 'SF Pro SemiBold';
+  font-size: 18px;
+  color: ${ThemeColors.darkGrey};
+`;
+
+export const CardFooter = styled.div`
+  background-color: ${ThemeColors.bg};
+  border-radius: 0px 0px 30px 30px;
+  box-shadow: 0px 3px 7px rgba(0, 0, 0, 0.16);
+  height: 50px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  align-self: flex-end;
+  padding: 0px 25px;
+`;
+
+export const FooterBtn = styled.button`
+  cursor: pointer;
+  border: none;
+  text-decoration: none;
+  transition: all 0.2s ease-in-out;
+  display: flex;
+  align-items: center;
+  span {
+    font-family: 'SF Pro Medium';
+    color: ${ThemeColors.lightBlue};
+    font-size: 14px;
+    margin-left: 8px;
+  }
+
+  &:active,
+  &:focus {
+    outline: none;
+  }
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+
+export const PokeTypes = styled.span`
+  background-color: ${ThemeColors.bg};
+  padding: 5px 20px;
+  border-radius: 25px;
+  font-family: 'SF Pro Light';
+  font-size: 13px;
+  margin-right: 8px;
+`;
+
+export const PokeImg = styled.img`
+  position: absolute;
+  top: -35px;
+  right: 35px;
+  height: 90px;
+`;
