48a2b3cbac1d3ef76159e742ce81b08eafaec536
Weslen Breno <weslenbreno@gmail.com>
2020-12-04 02:35:09 -0300

    Lazy Loading to routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import 'react-toastify/dist/ReactToastify.css';
-import { Details, FavoritesPage, Home } from 'pages';
-import React from 'react';
+import React, { Suspense } from 'react';
 import { ThemeColors } from 'shared/constants/Colors';
 import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
@@ -8,6 +7,11 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { ToastContainer } from 'react-toastify';
 import store, { persistor } from 'store/store';
+import { Loading } from 'components';
+
+const Home = React.lazy(() => import('pages/Home/Home'));
+const Details = React.lazy(() => import('pages/Details/DetailsPage'));
+const Favorites = React.lazy(() => import('pages/Favorites/Favorites'));
 
 const AppContainer = styled.div`
   min-height: 100vh;
@@ -21,18 +25,20 @@ function App() {
     <AppContainer>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <Switch>
-            <Route path="/favorites">
-              <FavoritesPage />
-            </Route>
-            <Route exact path="/detail/:id">
-              <Details />
-            </Route>
-            <Route exact path="/">
-              <Home />
-            </Route>
-          </Switch>
-          <ToastContainer />
+          <Suspense fallback={<Loading />}>
+            <Switch>
+              <Route path="/favorites">
+                <Favorites />
+              </Route>
+              <Route exact path="/detail/:id">
+                <Details />
+              </Route>
+              <Route exact path="/">
+                <Home />
+              </Route>
+            </Switch>
+            <ToastContainer />
+          </Suspense>
         </PersistGate>
       </Provider>
     </AppContainer>
